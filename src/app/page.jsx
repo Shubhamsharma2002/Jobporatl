7800e695576cc42dec316490b5e7ab0406ef89ac
shubham sharma <shubhamtech004@gmail.com>
2025-07-27 23:43:06 +0530

    setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="bg-amber-700 text-2xl text-amber-50">
-      job portal coming up ..................
+      job portal coming up ..................   
     </div>
   );
 }
