a5f3d4ef8288f69629498157f083e9169fc0843c
shubham sharma <shubhamtech004@gmail.com>
2025-08-18 23:26:48 +0530

    vercel blog setup done

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,11 +1,11 @@
 export const uploadToBlob = async (file) => {
   const formData = new FormData();
-  formData.append('file', file);
+  formData.append("file", file);
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/upload`, {
-    method: 'POST',
+    method: "POST",
     body: formData, // ✅ FormData automatically sets multipart/form-data
   });
 
   return res.json();
-}
+};
