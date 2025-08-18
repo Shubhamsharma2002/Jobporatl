7ea36c6210a09c369797a78d3fb07165b74a0058
shubham sharma <shubhamtech004@gmail.com>
2025-08-18 23:27:18 +0530

    vercel blog setup done

--- src/app/api/v1/upload/route.js
+++ src/app/api/v1/upload/route.js
@@ -1,22 +1,23 @@
-import { put } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+import { put } from "@vercel/blob";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const origin = request.headers.get('origin');
-  if(origin !== process.env.NEXT_PUBLIC_BASE_URL) 
+  const origin = request.headers.get("origin");
+  if (origin !== process.env.NEXT_PUBLIC_BASE_URL)
     return NextResponse.json({ message: "Couldn't upload" }, { status: 403 });
 
   // Read file from FormData
   const formData = await request.formData();
-  const file = formData.get('file');
-  if (!file) return NextResponse.json({ message: "No file uploaded" }, { status: 400 });
+  const file = formData.get("file");
+  if (!file)
+    return NextResponse.json({ message: "No file uploaded" }, { status: 400 });
 
   // Upload inside "resumes/" folder
   const blob = await put(`resumes/${Date.now()}-${file.name}`, file, {
-    access: 'public',
+    access: "public",
     token: process.env.BLOB_READ_WRITE_TOKEN,
   });
-  console.log("blog resposne",blob);
-  
+  console.log("blog resposne", blob);
+
   return NextResponse.json(blob);
 }
